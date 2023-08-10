d4cb364f86f775b34ff33a0c5d09211e58e2ad86
kiran mh <kiranmh@gmail.com>
2023-08-11 02:24:32 +0530

    etrraform code to create EC2

---
+++ ec2-v2.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "demo-server" {
+  ami = "ami-08a52ddb321b32a8c"
+  instance_type = "t2.micro"
+  key_name = "devops"
+  security_groups = [ "demo-sg" ]
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH access"
+  
+
+  ingress {
+    description      = "SSH access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [ "0.0.0.0/0" ]
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "demo-sg"
+  }
+}
